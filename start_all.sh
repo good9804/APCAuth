c5b052e26ed1efc014a638873c7fd69ce93863a0
Korjw <fkrk56@naver.com>
2023-06-07 16:02:08 +0900

    2023_06_07

---
+++ start_all.sh
@@ -0,0 +1,21 @@
+cd ..
+cd APCAuth
+sudo sh start.sh &
+
+cd ..
+cd APCDashBoard
+sudo sh start.sh &
+
+cd ..
+cd APCProduct
+sudo sh start.sh &
+
+cd ..
+cd APCOrder
+sudo sh start.sh &
+
+cd ..
+cd APCKafka
+sudo sh start.sh &
+
+cd ..
