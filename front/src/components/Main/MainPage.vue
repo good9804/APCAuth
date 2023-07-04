4ed71fa8f4144865a0e364d1ef4d7588eed26866
good9804 <ggoggoni98@gmail.com>
2023-07-04 16:12:09 +0900

    logo 추가, 하이퍼링크 추가

--- front/src/components/Main/MainPage.vue
+++ front/src/components/Main/MainPage.vue
@@ -2,32 +2,53 @@
   <div class="flex-wrap p-2 sm:ml-64">
     <div class="p-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
       <div class="max-w-[56rem] min-w-[18rem] mx-auto">
+        <div class="grid md:grid-cols-2 md:gap-6 relative z-0 mb-6 w-full group;">
+          <img src="../../assets/nhLogo.png" >
+          <img src="../../assets/chungjuLogo.png" >
+        </div>
+        
         <div class="grid md:grid-cols-3 md:gap-6 relative z-0 mb-6 w-full group;">
-          <p class="text-white col-span-2 text-xl">
-            농림축산식품부 인공지능 기반 신선과채류의 품질 판정 기술 개발 사업으로<br>
+          <p class="text-white col-span-2 text-2xl">
+            농림축산식품부 인공지능 기반 신선 과채류의 품질 판정 기술 <br>개발 사업으로<br>
             디지털 트윈 시스템 개발중인 사이트입니다.<br>
+            <br>
             충주 APC를 디지털트윈화 하였으며 <br>
             아래 동영상은 APC 전체 공정과정을 나타낸 것 입니다.<br>
             <br>
+            <br>
+            <br>
+            <br>
             현재 사이트에서 제공하는 기능들은 오른쪽 목록과 같습니다.<br><br>
-            <img src="/users/img/apcRoad.00424625.png" width="1000" height="600">
           </p>
           <div class="flex justify-end">
             <ul class="text-white">
-              <li> 입고
-                <p> - 회원 별 입고 기록 관리 </p>
+              <li class="text-xl">
+                <a href="https://068e-118-36-223-138.ngrok-free.app/product/import/view">
+                <p class="underline">
+                  입고</p></a>
+                <p class="text-base">- 회원 별 입고 기록 관리</p>
               </li>
-              <li> 모니터링
-                <p> - 공정 프로세스 3D 모델링<br>
+              <li class="text-xl">
+                <a href="https://068e-118-36-223-138.ngrok-free.app/dashboard">
+                  <p class="underline">
+                모니터링</p></a>
+                <p class="text-base">
+                  - 공정 프로세스 3D 모델링<br>
                   - 선별 진행 상황<br>
                   - 저장고 구역별 온,습도<br>
                   - 이상상태 알림<br>
                 </p>
               </li>
-              <li> 저장소
-                <p> - 저장고 구역 관리 </p>
+              <li class="text-xl">
+                <a href="https://068e-118-36-223-138.ngrok-free.app/dashboard/storage">
+                  <p class="underline">저장소</p></a>
+                <p class="text-base">- 저장고 구역 관리</p>
               </li>
-              <li> 유통 <p> - 상품 등록<br>
+              <li class="text-xl">
+                <a href="https://068e-118-36-223-138.ngrok-free.app/order/main">
+                  <p class="underline">유통</p></a>
+                <p class="text-base">
+                  - 상품 등록<br>
                   - 주문 등록<br>
                   - 회원 문의<br>
                   - 주문 조회<br>
@@ -36,12 +57,9 @@
             </ul>
           </div>
         </div>
-        <div class="grid md:grid-cols-2 md:gap-6 relative z-0 mb-6 w-full group align-items: center;">
-          <object width="800" height="400" data="https://www.youtube.com/embed/N3cPl0uZTh4" frameborder="0"
-            allowfullscreen="">
-
-          </object>
-        </div>
+        <img src="../../assets/apcRoad.png" width="1000" height="600">
+        <br>
+        <iframe width="1000" height="400" src="https://www.youtube.com/embed/N3cPl0uZTh4" frameborder="0" allowfullscreen></iframe>
       </div>
     </div>
   </div>
@@ -56,5 +74,3 @@ export default {
   }
 };
 </script>
-
-
