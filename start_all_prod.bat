81d8bb7ea41430d326340d7600f3abba2cbb082a
good9804 <ggoggoni98@gmail.com>
2023-07-31 11:38:48 +0900

    일괄실행가능 bat파일 추가

---
+++ start_all_prod.bat
@@ -0,0 +1,11 @@
+@echo off
+cd /d %~dp0
+REM the example of running batch files in sequence and in the background
+
+start /B "" cmd /c call start_product.bat
+start /B "" cmd /c "cd  ..\APCKafka && call kafka.bat"
+start /B "" cmd /c "cd  ..\APCDashBoard && call start_product.bat"
+start /B "" cmd /c "cd  ..\APCProduct && call start_product.bat"
+start /B "" cmd /c "cd  ..\APCOrder && call start_product.bat"
+
+pause
